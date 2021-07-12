d0f83634c7e16358bafd60c9b534a5f07001b9ec
gagf01 <francis.gagne@sct.gouv.qc.ca>
2021-07-12 11:01:53 -0400

    API Dockerfile
    Signed-off-by: gagf01 <francis.gagne@sct.gouv.qc.ca>

--- third_party/genswagger.sh
+++ third_party/genswagger.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
 
 go get github.com/rakyll/statik
 
-cp ../api/openapi-v1.yaml ./swaggerui/swagger.yaml
+cp ${SCRIPTPATH}/../api/openapi-v1.yaml ${SCRIPTPATH}/swaggerui/swagger.yaml
 
-statik -src=./swaggerui -p=swaggerui -f
+statik -src=${SCRIPTPATH}/swaggerui -p=swaggerui -f -dest=${SCRIPTPATH}
 
-mv ./swaggerui/statik.go ../api/swaggerui.go
+mv ${SCRIPTPATH}/swaggerui/statik.go ${SCRIPTPATH}/../api/swaggerui.go
